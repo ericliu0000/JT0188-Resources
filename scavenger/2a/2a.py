09b9e7ed52bb9435c442f5fb124ac16cfa2f238b
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2023-01-16 20:22:39 -0500

    add challenge 2a

---
+++ scavenger/2a/2a.py
@@ -0,0 +1,15 @@
+from manim import *
+
+class Animation(Scene): 
+    def construct(self):
+        mobject = Circle(3, color=GREEN_C)
+        text = Text("Here is my equation").shift(DOWN * 2)
+        equation = MathTex("f(x)").move_to(mobject, UP)
+
+        self.play(Create(mobject))
+        self.play(mobject.animate().shift(LEFT * 3))
+        self.play(Write(text))
+        self.play(Create(equation))
+        self.wait(2)
+        self.play(FadeOut(mobject, text, equation))
+        self.wait(1)
