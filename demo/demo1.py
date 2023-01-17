32373da1dee728e4a16c191d4056c0243ccee80b
Ben Caunt <bdcaunt@gmail.com>
2023-01-17 09:18:26 -0500

    add demo1

---
+++ demo/demo1.py
@@ -0,0 +1,36 @@
+from manim import *
+
+
+'''
+Either one of ``"spring"`` (the default), ``"circular"``, ``"kamada_kawai"``,
+``"planar"``, ``"random"``, ``"shell"``, ``"spectral"``, ``"spiral"``, ``"tree"``, and ``"partite"``
+
+'''
+# class name
+class Example1(Scene):
+    def construct(self):
+        
+        # create a path graph of 6 vertices, with circular layout
+        g = Graph([0, 1, 2, 3, 4, 5], [(0,1),(1,2),(2,3),(3,4),(4,5)],layout="spring")
+                # create a path graph of 6 vertices, with spring layout
+        g2 = Graph([0, 1, 2, 3, 4, 5], [(0,1),(1,2),(2,3),(3,4),(4,5)],layout="circular")
+
+        # draw the graph
+        self.play(Create(g))
+
+        # highlight each vertex
+        for i in range(6):
+            self.play(Indicate(g.vertices[i],color=BLUE,radius=5))
+            self.wait(0.2)
+
+        # wait for 2 seconds
+        self.wait(2)
+        # draw the graph
+        self.play(ReplacementTransform(g,g2))
+        # wait for 2 seconds
+        self.wait(2)
+        for i in range(6):
+            self.play(Indicate(g.vertices[i],color=RED,radius=5))
+            self.wait(0.05)
+        self.wait(2)
+
