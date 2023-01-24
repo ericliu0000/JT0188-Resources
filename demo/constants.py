c8eabce4869dd4f9c1d0db2310cb39f15375c4d0
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2023-01-23 22:50:56 -0500

    Add time demo

---
+++ demo/constants.py
@@ -0,0 +1,196 @@
+from manim import *
+import math
+
+# Next text time in cycles
+NEXT_TEXT_TIME = 4
+
+# Animation delay time in seconds
+ANIMATION_TIME = 0.8
+
+# Fox and hare file names
+FOX_FILE_NAME = "assets/foxBased.svg"
+HARE_FILE_NAME = "assets/hareBased.svg"
+
+# Fox and hare scales
+FOX_SCALE = 0.6
+HARE_SCALE = 0.48
+
+# Text buffers in units
+TOP_TEXT_BUFFER = 0.4
+BETWEEN_TEXT_BUFFER = 0.2
+
+# Animation wait time in seconds
+PAUSE_TIME = 0.8
+LONG_PAUSE_TIME = 5
+
+# Text color as a Manim color
+TEXT_COLOR = RED
+INDICATION_COLOR = YELLOW
+
+# Helper function to generate animations in bulk for indication
+
+
+def bulk_indicate(graph: Graph, edges: list):
+    actions = []
+
+    for edge in edges:
+        actions.append(Indicate(graph.edges[edge], color=INDICATION_COLOR))
+
+    return actions
+
+
+def bulk_indicate_points(graph: Graph, points: list):
+    actions = []
+
+    for point in points:
+        actions.append(Indicate(graph.vertices[point], color=INDICATION_COLOR))
+
+    return actions
+
+
+class CycleGraphConstants:
+    GRAPH_OFFSET = UP * 0.5
+
+    VERTICES = [1, 2, 3, 4, 5, 6]
+    EDGES = [(1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 1)]
+    LAYOUT = {1: [-2, 1.3, 0], 2: [0, 2.3, 0], 3: [2, 1.3, 0],
+              4: [2, -0.7, 0], 5: [0, -1.7, 0], 6: [-2, -0.7, 0]}
+
+    ONE_FOX_FRAMES = [i for i in range(4, 25, 4)]
+
+class ThreeCycleGraphConstants:
+    TEXT_SHIFT = UP * 2.5
+
+    VERTICES = [1, 2, 3]
+    EDGES = [(1, 2), (2, 3), (3, 1)]
+    LAYOUT = {1: [-2, 0, 0], 2: [0, -2.4, 0], 3: [2, 0, 0]}
+
+class WheelGraphConstants:
+    VERTICES = [1, 2, 3, 4, 5, 6, 7]
+    EDGES = [(1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 1),
+             (1, 7), (2, 7), (3, 7), (4, 7), (5, 7), (6, 7)]
+    LAYOUT = {1: [-2, 1.3, 0], 2: [0, 2.3, 0], 3: [2, 1.3, 0],
+              4: [2, -0.7, 0], 5: [0, -1.7, 0], 6: [-2, -0.7, 0], 7: [0, 0.3, 0]}
+
+
+class PathGraphConstants:
+    VERTICES = [1, 2, 3]
+    EDGES = [(1, 2), (2, 3)]
+    LAYOUT = {1: [-2, 0, 0], 2: [0, 0, 0], 3: [2, 0, 0]}
+
+    TEXT_SHIFT = DOWN * 2
+
+
+class SubgraphConstants:
+    TEXT_SHIFT = DOWN * 2.5
+
+    VERTICES = [1, 2, 3, 4, 5, 6, 7, 8, 11, 13, 16, 18]
+    EDGES = [(11, 13), (2, 11), (2, 13),
+             (1, 3), (3, 4), (4, 5), (5, 6), (6, 8), (8, 1),
+             (16, 18), (18, 7), (7, 16)]
+
+    FULL_LAYOUT = {
+        1: [-1, 2, 0], 2: [1, 2, 0], 3: [2, 1, 0],
+        4: [2, -1, 0], 5: [1, -2, 0], 6: [-1, -2, 0],
+        7: [-2, -1, 0], 8: [-2, 1, 0], 11: [-1, 2, 0],
+        13: [2, 1, 0], 16: [-1, -2, 0], 18: [-2, 1, 0]
+    }
+
+    EXPLODED_LAYOUT = {
+        1: [-1, 2, 0], 2: [1, 2.5, 0], 3: [2, 1, 0],
+        4: [2, -1, 0], 5: [1, -2, 0], 6: [-1, -2, 0],
+        7: [-2.5, -1, 0], 8: [-2, 1, 0], 11: [-1, 2.5, 0],
+        13: [2, 1.5, 0], 16: [-1.5, -2, 0], 18: [-2.5, 1, 0]
+    }
+
+    MOVES = [(2, 6), (1, 5), (8, 4), (6, 3), (5, 1), (4, 8)]
+
+
+class AltSubgraphConstants:
+    VERTICES = [i for i in range(1, 15)] + [i for i in range(101, 111)]
+    EDGES = [(1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 9),
+             (9, 10), (10, 11), (11, 12), (12, 13), (13, 14), (14, 1), (101, 102),
+             (102, 103), (103, 104), (104, 105), (105, 106), (106, 107),
+             (107, 108), (108, 109), (109, 110), (110, 101), (1, 101), (2, 101),
+             (2, 102), (3, 102), (4, 102), (4, 103), (5, 103), (6, 103), (6, 104),
+             (6, 105), (7, 105), (8, 105), (8, 106), (8, 107), (9, 107), (10, 107),
+             (10, 108), (11, 108), (11, 109), (12, 109), (13, 109), (13, 110),
+             (14, 110), (1, 110)]
+
+    FULL_LAYOUT = {}
+
+    for i in range(1, 15):
+        FULL_LAYOUT[i] = [
+            1.5 * math.cos(math.pi * i / 7), 1.5 * math.sin(math.pi * i / 7), 0]
+
+    for i in range(101, 111):
+        FULL_LAYOUT[i] = [math.cos(
+            (math.pi * i / 5) + math.pi / 40), math.sin((math.pi * i / 5) + math.pi / 40), 0]
+
+    SPLIT_LAYOUT = {}
+
+    for i in range(101, 111):
+        SPLIT_LAYOUT[i] = [
+            1.5 * math.cos(math.pi * i / 5), 1.5 * math.sin(math.pi * i / 5), 0]
+
+
+class OptimizationConstants:
+    ANIMATION_TIME = 3
+
+    TEXT_SHIFT = UP * 2.5
+    GRAPH_SHIFT = DOWN * 1.0
+
+    GRAPH_1_VERTICES = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    GRAPH_1_EDGES = [(0, 2), (1, 0), (1, 2), (0, 3), (1, 4), (2, 5),
+                     (5, 6), (6, 7), (1, 8), (8, 9), (9, 10)]
+
+    GRAPH_2_VERTICES = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    GRAPH_2_EDGES = [(0, 1), (1, 2), (2, 3), (0, 3), (4, 5),
+                     (5, 6), (4, 6), (6, 7), (7, 8), (8, 9), (9, 10),
+                     (3, 4), (0, 11), (11, 12), (12, 13)]
+
+
+class OptimalPlacementConstants:
+    VERTICES = [1, 2, 3, 4, 5, 6, 7]
+    EDGES = [(1, 2), (2, 3), (3, 1), (4, 5), (5, 6), (6, 7), (7, 4), (3, 4)]
+
+    LAYOUT = {1: [-3, -1.5, 0], 2: [-3, 1.5, 0],  3: [-1, 0, 0],
+              4: [1, 0, 0], 5: [2.5, 1.5, 0], 6: [4, 0, 0], 7: [2.5, -1.5, 0]}
+
+    TEXT_SHIFT = DOWN * 2.5
+
+    MOVES = [[5, 3], [6, 4], [5, 7], [6, 4], [5, 7], [6, 4], [5, 7]]
+
+
+class ProblemGraphConstants:
+    VERTICES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+                20, 21, 22, 23, 24,
+                101, 102, 103, 104, 105, 106]
+    
+    EDGES = [(1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 1),
+            (7, 8), (8, 9), (9, 10), (10, 11), (11, 12), (12, 7),
+            (13, 14), (14, 15), (15, 16), (16, 17), (17, 18), (18, 13),
+            (1, 20), (20, 21), (21, 22), (22, 23), (23, 24),
+            (101, 102), (102, 103), (103, 104), (104, 105), (105, 106), (106, 101)]
+
+    FULL_LAYOUT = {1: [0, 1.5, 0], 2: [1.5, 0, 0], 3: [3, -1.5, 0], 
+            4: [1.5, -1, 0], 5: [0.75, -0.25, 0], 6: [0, 0.5, 0],
+            7: [3, -1.5, 0], 8: [0, -1.5, 0], 9: [-3, -1.5, 0],
+            10: [-1.5, -1, 0], 11: [0, -1, 0], 12: [1.5, -1, 0],
+            13: [-3, -1.5, 0], 14: [-1.5, 0, 0], 15: [0, 1.5, 0],
+            16: [0, 0.5, 0], 17: [-0.75, -0.25, 0], 18: [-1.5, -1, 0],
+            20: [0.2, 2, 0], 21: [0.7, 2.5, 0], 22: [1.2, 2.6, 0],
+            23: [1.7, 2.5, 0], 24: [2.2, 2, 0],
+            101: [0, 0.5, 0], 102: [0.75, -0.25, 0], 103: [1.5, -1, 0],
+            104: [0, -1, 0], 105: [-1.5, -1, 0], 106: [-0.75, -0.25, 0]}
+
+    SPLIT_LAYOUT = {1: [0.5, 1.5, 0], 2: [2, 0, 0], 3: [3.5, -1.5, 0],
+            4: [2, -1, 0], 5: [1.25, -0.25, 0], 6: [0.5, 0.5, 0],
+            7: [3, -2, 0], 8: [0, -2, 0], 9: [-3, -2, 0],
+            10: [-1.5, -1.5, 0], 11: [0, -1.5, 0], 12: [1.5, -1.5, 0],
+            13: [-3.5, -1.5, 0], 14: [-2, 0, 0], 15: [-0.5, 1.5, 0],
+            16: [-0.5, 0.5, 0], 17: [-1.25, -0.25, 0], 18: [-2, -1, 0],
+            20: [0.2, 2, 0], 21: [0.7, 2.5, 0], 22: [1.2, 2.6, 0],
+            23: [1.7, 2.5, 0], 24: [2.2, 2, 0],
+            101: [0, 0.5, 0], 102: [0.75, -0.25, 0], 103: [1.5, -1, 0],
+            104: [0, -1, 0], 105: [-1.5, -1, 0], 106: [-0.75, -0.25, 0]}
--- demo/demo3.py
+++ demo/demo3.py
@@ -4,18 +4,15 @@ class MDemo(Scene):
     def construct(self):
         # Make path and dot objects
         path = VMobject()
-        dot = Dot().move_to((-2, -1, 0))
+        dot = Dot().move_to((-1, -1, 0))
         path.set_points_as_corners([dot.get_center(), dot.get_center()])
 
-        # Make the path a foreground object to make it look (and feel!) cooler
-        self.add_foreground_mobjects(path)
-
         # Create four paths in the letter "M"
-        l1 = Line((-2, -1, 0), (-2, 1, 0))
-        l2 = Line((-2, 1, 0), (0, -1, 0))
-        l3 = Line((0, -1, 0), (2, 1, 0))
-        l4 = Line((2, 1, 0), (2, -1, 0))
-
+        cheese = Line((-1, -1, 0), (-1, 2, 0))
+        l1 = Line((-1, 2, 0), (0, -1, 0))
+        lines = Line((0, -1, 0), (1, 2, 0))
+        cheese2 = Line((1, 2, 0), (1, -1.25, 0))
+        
         # Create path updater and add to path
         def update_path(path):
             previous_path = path.copy()
@@ -27,10 +24,10 @@ class MDemo(Scene):
         self.play(Create(path), Create(dot))
 
         # Move 
+        self.play(MoveAlongPath(dot, cheese))
         self.play(MoveAlongPath(dot, l1))
-        self.play(MoveAlongPath(dot, l2))
-        self.play(MoveAlongPath(dot, l3))
-        self.play(MoveAlongPath(dot, l4))
+        self.play(MoveAlongPath(dot, lines))
+        self.play(MoveAlongPath(dot, cheese2))
 
         # Destroy the dot from the face of the earth
         self.play(Uncreate(dot))
@@ -39,20 +36,21 @@ class MDemo(Scene):
         path.remove_updater(update_path)
 
         # Scale the "M" down, and move it down
-        self.play(path.animate().scale(0.5).shift(DOWN))
+        self.play(path.animate().scale(0.5).shift(DOWN + LEFT))
 
         # Do the pixar lamp animation with the "M," but be lazy about it
         # Make a circle object to be the lamp and move it to an okay location
-        lamp = Circle(1, fill_opacity=1).move_to((0, 2, 0))
+        lamp = Circle(0.5, fill_opacity=1).move_to((-1, 1, 0))
         self.play(Create(lamp))
+        self.play(lamp.animate(rate_func=rate_functions.ease_out_bounce).shift(DOWN * 2))
 
         # Make the lamp bounce on top of the object and end up inside it, then wait a bit
-        self.play(lamp.animate(run_time=3, rate_func=rate_functions.ease_out_bounce).shift(DOWN * 2.5))
-        self.wait(2)
+        self.wait(1)
 
         # New scene! Move everything to the side to make a bar chart with various metrics
-        lamp_path = Group(lamp, path)
-        self.play(lamp_path.animate().shift(LEFT * 5))
+        group = Group(path, lamp)
+        # self.play(path.animate().shift(LEFT * 4), lamp.animate().shift(LEFT * 4))
+        self.play(group.animate().shift(LEFT * 4))
 
         # Create a bar chart object and populate it with the following metrics:
         # lines: 4
@@ -60,24 +58,21 @@ class MDemo(Scene):
         # manim: 10
         # yes: 2
         # Then, show it (and the labels) to the screen
-
         chart = BarChart(
-            values=[4, 3, 10, 2],
-            bar_names=["lines", "hours slept", "manim", "yes"],
-            y_range=[0, 10, 1],
-            y_length=6,
-            x_length=8,
-            x_axis_config={"font_size": 36},
+            values = [4, 3, 10, 2],
+            bar_names = ["lines", "hours slept", "manim", "yes"],
+            y_range = [0, 15, 2],
+            y_length = 6,
+            x_length = 10,
+            x_axis_config={"font_size": 36}
         ).shift(RIGHT * 2)
+        c_bar_lbls = chart.get_bar_labels(font_size=48)
 
-        c_bar_lbls = chart.get_bar_labels(font_size=36)
-        self.play(Create(chart), Create(c_bar_lbls))
+        self.play(FadeIn(chart), FadeIn(c_bar_lbls))
 
         self.wait(3)
 
         # Break everything apart using Uncreate
-        self.play(Uncreate(chart), Uncreate(c_bar_lbls))
+        self.play(Unwrite(chart), Unwrite(c_bar_lbls))
         self.play(Uncreate(path))
         self.play(Uncreate(lamp))
-
-        self.wait(0.5)
